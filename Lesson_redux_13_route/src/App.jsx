b5c6a02cd0defe531f0e6b340d7c73344917745f
IrinAnoVo <irinapavelviktor@gmail.com>
2025-05-28 01:43:19 +0200

    new

--- Lesson_redux_13_route/src/App.jsx
+++ Lesson_redux_13_route/src/App.jsx
@@ -1,24 +1,35 @@
 import { Routes, Route, NavLink } from "react-router";
+import Home from "./pages/Home/home";
+import About from "./pages/About/about";
+import Posts from "./pages/Post/post";
+import Post from "./pages/Post/post";
+
+const setActiLink = ({ isActive }) => {
+  return isActive ? "navbar__item navbar__item-active" : "navbar__item";
+}
+
 const App = () => {
   return (
     <>
       <header>
         <nav className="navbar">
           <ul className="navbar-list">
-            <li><NavLink to="/" className="navbar__item ">Home</NavLink></li>
-            <li><NavLink to="/about" className="navbar__item navbar__item-active">About</NavLink></li>
-            <li><NavLink to="/posts" className="navbar__item">Posts</NavLink></li>
+            <li><NavLink to="/" className={setActiLink}>Home</NavLink></li>
+            <li><NavLink to="/about" className={setActiLink}>About</NavLink></li>
+            <li><NavLink to="/posts" className={setActiLink}>Posts</NavLink></li>
           </ul>
         </nav>
       </header>
       <main>
           <Routes>
-              <Route path="/" element={<h1>Home</h1>}/>
-              <Route path="/about" element={<h1>About</h1>}/>
-              <Route path="/posts" element={<h1>Posts</h1>}/>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/posts" element={<Posts/>}/>
+              <Route path="/posts/:postId" element={<Post/>}/>
+              <Route path="*" element={<h1>Page not found</h1>}/>
           </Routes>
       </main>
     </>
-  )
+  ) 
 }
 export default App
