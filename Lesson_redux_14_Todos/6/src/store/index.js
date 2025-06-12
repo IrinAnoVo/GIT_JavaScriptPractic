e0e13d4e60c2966f82be8bb6d11704fe1f8e2554
IrinAnoVo <irinapavelviktor@gmail.com>
2025-06-12 13:33:51 +0200

    new

--- Lesson_redux_14_Todos/6/src/store/index.js
+++ Lesson_redux_14_Todos/6/src/store/index.js
@@ -1,11 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from "./tasksSlice";
-import filterReducer from "./filterSlice";
-
-export const store = configureStore({
-    reducer: {
-        tasks: tasksReducer,
-        filter: filterReducer,
-    },
-});
 
